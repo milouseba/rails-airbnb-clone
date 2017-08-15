e3a9a5c4980dd3fb18d6d0dc46eceb1598eb074e
sebastien milou <milouseba@gmail.com>
2017-08-15 12:16:50 +0200

    ajout de css et view card

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+article = Article.new(title: "super weed", description: "super defonce", address: "16 villa gaudelet 75011 Paris",
+                      price: rand(30..60), user_id: 2)
+
+article.save
